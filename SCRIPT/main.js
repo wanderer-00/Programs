fe475e2e39e06a40d96ad0398502b86f30304ab1
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2023-06-29 19:28:20 +0300

    -

--- SCRIPT/main.js
+++ SCRIPT/main.js
@@ -14,7 +14,7 @@ while (n < numberOBJ) {
                 <div class="ICON" style="background-image: url(IMG/${ARRAY[n].ICON})"></div>
                 <div class="NAME">${ARRAY[n].NAME}</div>
                 <div class="BUTTONS">
-                    <a class="LOAD" download href="EXE/${ARRAY[n].EXE}" title="Скачать с нашего сайта">Скачать</a>
+                    <a class="LOAD" download href="EXE/${ARRAY[n].EXE}.7z" title="Скачать с нашего сайта">Скачать</a>
                     <a class="URL" href="${ARRAY[n].URL}" title="Перейти на официальный сайт">↑</a>
                     <!--<object type="image/svg+xml" width="30px" height="30px" data="IMG/ICON/open_in_new.svg" id="object" class="icon"></object>-->
                 </div>
